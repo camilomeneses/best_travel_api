93ccbca6d6c0bfd9190b79a44313a0faf476b944
Camilo Meneses <kamilomeneses@gmail.com>
2023-07-26 18:20:55 -0500

    mapping entity to DTO Response

--- src/main/java/dev/camilo/demo/infraestructure/services/TicketService.java
+++ src/main/java/dev/camilo/demo/infraestructure/services/TicketService.java
@@ -1,12 +1,31 @@
 package dev.camilo.demo.infraestructure.services;
 
 import dev.camilo.demo.api.models.request.TicketRequest;
+import dev.camilo.demo.api.models.responses.FlyResponse;
 import dev.camilo.demo.api.models.responses.TicketResponse;
+import dev.camilo.demo.domain.entities.Ticket;
+import dev.camilo.demo.domain.repositories.CustomerRepository;
+import dev.camilo.demo.domain.repositories.FlyRepository;
+import dev.camilo.demo.domain.repositories.TicketRepository;
 import dev.camilo.demo.infraestructure.abstract_services.ITicketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@Transactional //gestion de transacciones
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TicketService implements ITicketService {
+
+  private final FlyRepository flyRepository;
+  private final CustomerRepository customerRepository;
+  private final TicketRepository ticketRepository;
+
   @Override
   public TicketResponse create(TicketRequest request) {
     return null;
@@ -25,5 +44,15 @@ public class TicketService implements ITicketService {
   @Override
   public void delete(UUID id) {
 
+  }
+
+  //mapeo de Entity a DTOResponse
+  private TicketResponse entityToResponse(Ticket entity){
+    var response = new TicketResponse();
+    BeanUtils.copyProperties(entity,response);
+    var flyResponse = new FlyResponse();
+    BeanUtils.copyProperties(entity.getFly(),flyResponse);
+    response.setFly(flyResponse);
+    return response;
   }
 }
