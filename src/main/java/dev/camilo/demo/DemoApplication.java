862306281ce486c305251e47388296324ea983b9
Camilo Meneses <kamilomeneses@gmail.com>
2023-07-26 17:05:59 -0500

    testing implementation

--- src/main/java/dev/camilo/demo/DemoApplication.java
+++ src/main/java/dev/camilo/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package dev.camilo.demo;
 
+import dev.camilo.demo.domain.entities.Reservation;
+import dev.camilo.demo.domain.entities.Ticket;
+import dev.camilo.demo.domain.entities.Tour;
 import dev.camilo.demo.domain.repositories.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -58,7 +63,61 @@ public class DemoApplication implements CommandLineRunner {
 		/*hotelRepository.findByRatingGreaterThan(3).forEach(System.out::println);*/
 
 		//JPQL join method names
-		var hotel = hotelRepository.findByReservationsId(UUID.fromString("12345678-1234-5678-1234-567812345678")).get();
-		System.out.println(hotel);
+		/*var hotel = hotelRepository.findByReservationsId(UUID.fromString("12345678-1234-5678-1234-567812345678")).get();
+		System.out.println(hotel);*/
+
+		//customer
+		var customer = customerRepository.findById("GOTW771012HMRGR087").orElseThrow();
+		log.info("Client name: " + customer.getFullName());
+
+		//hotel
+		var hotel = hotelRepository.findById(3L).orElseThrow();
+		log.info("Hotel name: " + hotel.getName());
+
+		//fly
+		var fly = flyRepository.findById(11L).orElseThrow();
+		log.info("Fly: " + fly.getOriginName() + " - " + fly.getDestinyName());
+
+		//creacion de tour con customer con patron builder
+		var tour = Tour.builder()
+				.customer(customer)
+				.build();
+
+		//creacion de ticket con patron builder
+		var ticket = Ticket.builder()
+				.id(UUID.randomUUID())
+				.price(fly.getPrice().multiply(BigDecimal.TEN))
+				.arrivalDate(LocalDate.now())
+				.departureDate(LocalDate.now())
+				.purchaseDate(LocalDate.now())
+				.customer(customer)
+				.tour(tour)
+				.fly(fly)
+				.build();
+
+		//creacion de reservacion con patron builder
+		var reservation = Reservation.builder()
+				.id(UUID.randomUUID())
+				.dateTimeReservation(LocalDateTime.now())
+				.dateEnd(LocalDate.now().plusDays(2))
+				.dateStart(LocalDate.now().plusDays(1))
+				.hotel(hotel)
+				.customer(customer)
+				.tour(tour)
+				.totalDays(1)
+				.price(hotel.getPrice().multiply(BigDecimal.TEN))
+				.build();
+
+		System.out.println("---- Saving ----");
+		tour.addReservation(reservation);
+		tour.updateReservations();
+
+		tour.addTicket(ticket);
+		tour.updateTickets();
+		var tourSaved = this.tourRepository.save(tour);
+		System.out.println("---- Deleting ----");
+		Thread.sleep(8000);
+		this.tourRepository.deleteById(tourSaved.getId());
+
 	}
 }
