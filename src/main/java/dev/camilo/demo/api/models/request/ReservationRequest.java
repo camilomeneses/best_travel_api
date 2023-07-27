cb55ecbca950a39a4811e3779d1ce94ed22c76ef
Camilo Meneses <kamilomeneses@gmail.com>
2023-07-26 21:45:05 -0500

    create DTO's to reservation

---
+++ src/main/java/dev/camilo/demo/api/models/request/ReservationRequest.java
@@ -0,0 +1,17 @@
+package dev.camilo.demo.api.models.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ReservationRequest implements Serializable {
+  private String idClient;
+  private Long idHotel;
+}
