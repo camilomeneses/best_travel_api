8822eed4045863e688eaeef53eb7d33c5b05159a
Camilo Meneses <kamilomeneses@gmail.com>
2023-07-28 17:27:55 -0500

    add IdNotFoundException response json and xml

--- src/main/java/dev/camilo/demo/DemoApplication.java
+++ src/main/java/dev/camilo/demo/DemoApplication.java
@@ -3,6 +3,13 @@ package dev.camilo.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -13,3 +20,4 @@ public class DemoApplication {
   }
 
 }
+
---
+++ src/main/java/dev/camilo/demo/api/controllers/error_handler/BadRequestController.java
@@ -0,0 +1,49 @@
+package dev.camilo.demo.api.controllers.error_handler;
+
+import dev.camilo.demo.api.models.responses.error.BaseErrorResponse;
+import dev.camilo.demo.api.models.responses.error.ErrorResponse;
+import dev.camilo.demo.util.exceptions.IdNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * ControllerAdvice para interceptar un IdNotFoundException y responder
+ * con una estructura de ErrorResponse el cual tiene
+ * -
+ */
+@RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestController {
+
+  @ExceptionHandler(IdNotFoundException.class)
+  public ResponseEntity<BaseErrorResponse> handleIdNotFound(IdNotFoundException exception, HttpServletRequest request) {
+    String path = request.getRequestURI();
+    /*verificar si el path contiene /xml/*/
+    boolean isXmlRequest = path.contains("/xml/");
+
+    ErrorResponse errorResponse = ErrorResponse.builder()
+        .message(exception.getMessage())
+        .status(HttpStatus.BAD_REQUEST.name())
+        .errorCode(HttpStatus.BAD_REQUEST.value())
+        .build();
+
+    if (isXmlRequest) {
+      /*Si es una solicitud XML, responder con formato XML*/
+      return ResponseEntity.badRequest()
+          .contentType(MediaType.APPLICATION_XML)
+          .body(errorResponse);
+    } else {
+      /*Si no es una solicitud XML, responder con formato JSON (por defecto)*/
+      return ResponseEntity
+          .badRequest()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(errorResponse);
+    }
+  }
+}
+
